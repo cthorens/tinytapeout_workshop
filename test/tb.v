bbe55a2b15f905e41241afaf557feccdb36c81dd
cthorens <cthorens@student.ethz.ch>
2026-02-22 13:58:57 +0100

    Test to submit mini cpu

--- test/tb.v
+++ test/tb.v
@@ -15,24 +15,12 @@ module tb ();
 
   // Wire up the inputs and outputs:
   reg clk;
-  reg rst_n;
-  reg ena;
-  reg [7:0] ui_in;
-  reg [7:0] uio_in;
-  wire [7:0] uo_out;
-  wire [7:0] uio_out;
-  wire [7:0] uio_oe;
+  wire [7:0] w_reg3;
 
   // Replace tt_um_example with your module name:
   tt_um_cthorens_vgatest user_project (
-      .ui_in  (ui_in),    // Dedicated inputs
-      .uo_out (uo_out),   // Dedicated outputs
-      .uio_in (uio_in),   // IOs: Input path
-      .uio_out(uio_out),  // IOs: Output path
-      .uio_oe (uio_oe),   // IOs: Enable path (active high: 0=input, 1=output)
-      .ena    (ena),      // enable - goes high when design is selected
-      .clk    (clk),      // clock
-      .rst_n  (rst_n)     // not reset
+      .i_CLK    (clk),      // clock
+      .o_REG3   (w_reg3)
   );
 
 endmodule
